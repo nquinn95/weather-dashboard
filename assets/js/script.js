df8025cca97168d0704f52e6c78db0585b528fd4
nick quinn <nick.quinn@stadium1.com>
2022-04-11 21:09:47 -0400

    committing so i can test weather API in https protocol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 //global variables for citys that have been saved, the weather API URL and the API Key for open weather
 const cityHistory = [];
-const weatherApiUrl = "https://api.openweathermap.org/";
+//const weatherApiUrl = "https://api.openweathermap.org/";
 const apiKey = "33c18137c7eeada489c911f5adf21546";
 
 
@@ -9,3 +9,17 @@ const apiKey = "33c18137c7eeada489c911f5adf21546";
 //timezone for Day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
+
+var weatherApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=33c18137c7eeada489c911f5adf21546`;
+function  weatherData(weatherApiUrl){
+    fetch(weatherApiUrl)
+    .then(function(response){
+        response.json()
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+weatherData();
