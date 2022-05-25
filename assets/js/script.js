215733b48748f31d7655c01387063b2668cb6557
nick quinn <nick.quinn@stadium1.com>
2022-05-24 22:23:35 -0400

    Changed http to https.  also added functionality to site

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,54 @@ var cityHistory = [];
 const apiKey = "33c18137c7eeada489c911f5adf21546";
 let selCity; 
 
+//latitude and longitude
+
 
 //timezone for Day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
 
- var weatherApiUrlBase = `http://api.openweathermap.org/geo/1.0/direct?q=`;
-var baseURL2 = `&limit=5&appid=${apiKey}`;
+ var geoApi = `https://api.openweathermap.org/geo/1.0/direct?q=`;
+var geoURL = `&limit=5&appid=${apiKey}`;
 //getting data back from openweathermap
-function  weatherData(weatherApi){
-    fetch(weatherApi)
+function  cityData(geoURLBase){
+    fetch(geoURLBase)
     .then(function(response){
         return response.json()
 
     })
     .then(function(data){
-        console.log(data); 
+        //get longitude and latitude for the city that is searched
+        let latitude = data[0].lat;
+        let longitude = data[0].lon;
+
+
+        //call for the function searching for the weather
+        weatherData(latitude,longitude);
+
+        console.log(latitude);
+        console.log(longitude);
     })
 }
 
-// weatherData(weatherApiUrl);
+function weatherData(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data.main.temp);
+        console.log(data.weather[0].main)
+        $("#cityDis").append(userCity.value);
+        $("#cityWeather").append("Weather: " + data.weather[0].description);
+        $("#temp").append("Temperature: " + data.main.temp + "F");
+    
+    })
+
+}
+
+
 
 
 var userCity = document.getElementById('user-city');
@@ -36,11 +63,11 @@ cityForm.addEventListener('submit', function (e) {
 
     //prevent the normal submission of the form
     e.preventDefault();
-    weatherData(weatherApiUrlBase +`${userCity.value}` + baseURL2);
+    cityData(geoApi +`${userCity.value}` + geoURL);
+
     console.log(userCity.value);
     
     cityHistory.push(userCity.value);
-
     localStorage.setItem("Cities", JSON.stringify(cityHistory));
     JSON.parse(localStorage.getItem("Cities"));
 });
